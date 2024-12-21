79157ce8fe5ca9a22b72cbf8922f23eafbf1fbc7
kovendhan5 <kovendhanofficial5@gmail.com>
2024-12-21 17:20:02 +0530

    wallpaper update

--- website/src/components/WallpaperGrid.jsx
+++ website/src/components/WallpaperGrid.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import '../styles/WallpaperGrid.css';
 
 const wallpapers = [
-  { id: 1, url: 'path/to/wallpaper1.jpg', title: 'Wallpaper 1' },
-  { id: 2, url: 'path/to/wallpaper2.jpg', title: 'Wallpaper 2' },
+  { id: 1, url: '/Asserts/wallpaperflare.com_wallpaper (1).jpg', title: 'Wallpaper 1' },
+  { id: 2, url: '/Asserts/wallpaperflare.com_wallpaper (3).jpg', title: 'Wallpaper 2' },
+  { id: 3, url: '/Asserts/wallpaperflare.com_wallpaper (2).jpg', title: 'Wallpaper 3' },
+  { id: 4, url: '/Asserts/wallpaperflare.com_wallpaper (4).jpg', title: 'Wallpaper 4' },
+  { id: 5, url: '/Asserts/wallpaperflare.com_wallpaper (5).jpg', title: 'Wallpaper 5' },
   // Add more wallpapers here
 ];
 
